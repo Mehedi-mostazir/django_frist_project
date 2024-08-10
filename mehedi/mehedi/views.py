0ad7c2c127258082d557af5363049fd508736551
MD Mehedi Mostazir <plabonmostazir@gmail.com>
2024-08-10 22:44:12 +0600

    Initial commit: Set up Django project

---
+++ mehedi/mehedi/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+  #return HttpResponse("Hello this is home")
+  return render(request,'index.html')
+
+def about(request):
+  return HttpResponse("This is about page")
+
+def contact(request):
+  return HttpResponse("This is my contact")
