df12080acb2f62598fbb465a66f592baf9a75c42
MD Mehedi Mostazir <plabonmostazir@gmail.com>
2024-08-11 23:08:06 +0600

    Frist app created and also set a layout file in mehedi

--- mehedi/fristapp/urls.py
+++ mehedi/fristapp/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
-    path('about/',views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('fristapp/',include('fristapp.urls')),
+    path('',views.all_fristapp, name='all_fristapp'),
 
 ]
---
+++ mehedi/fristapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def all_fristapp(request):
+  return render(request,'fristapp/all_fristapp.html')
