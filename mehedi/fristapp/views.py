cef27852485856b7271f3f36a7c7f3f1837e21af
MD Mehedi Mostazir <plabonmostazir@gmail.com>
2024-08-16 22:12:20 +0600

    working on db

--- mehedi/fristapp/views.py
+++ mehedi/fristapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import appvaraity
 
 
 # Create your views here.
 def all_fristapp(request):
+  apps = appvaraity.objects.all()
   return render(request,'fristapp/all_fristapp.html')
 
